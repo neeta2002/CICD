3fa5df30b5add0aad41a894a306239c05a857c40
neeta2002 <150219736+neeta2002@users.noreply.github.com>
2026-06-24 17:00:19 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import app
 def test_home_page():
     tester=app.test_client()
     response=tester.get("/")
-    assert response.status_code==200
+    assert response.status_code == 200
     assert b"Welcome to CI/CD Project" in response.data
